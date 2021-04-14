f1b38f0239f386277ea02cac5377bdb02b228da1
Quentin <qpreik@gmail.com>
2021-04-14 08:36:04 -0400

    BEWARE OF WIRES

--- mods-src/qptech/src/Electricity/BEEWire.cs
+++ mods-src/qptech/src/Electricity/BEEWire.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 
 namespace qptech.src
 {
     class BEEWire:BEElectric
     {
+        bool isInsulated = false;
+        public override void Initialize(ICoreAPI api)
+        {
+            base.Initialize(api);
+            isInsulated= Block.Attributes["isInsulated"].AsBool(isInsulated);
+
+        }
         public override void OnTick(float par)
         {
             base.OnTick(par);
             
         }
 
+        public virtual void EntityCollide(Entity entity)
+        {
+            if (!isInsulated && capacitor > 0) { entity.IsOnFire = true; }
+        }
 
     }
 }
---
+++ mods-src/qptech/src/Electricity/BlockWire.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Common.Entities;
+
+namespace qptech.src { 
+    class BlockWire:ElectricalBlock
+    {
+        public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact)
+        {
+            base.OnEntityCollide(world, entity, pos, facing, collideSpeed, isImpact);
+            var mywire= world.BlockAccessor.GetBlockEntity(pos) as BEEWire;
+            if (mywire != null) { mywire.EntityCollide(entity); }
+        }
+    }
+}
