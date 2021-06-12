56ef4a31bf8fd847a3734d29b70758405dfcb4cd
Quentin Preik <quentin@preik.net>
2021-06-12 10:08:07 -0400

    Added Heatsink model, macerator recipe for metal scraps.

---
+++ mods-src/qptech/src/Electricity/BEEChiller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qptech.src.Electricity
+{
+    /// <summary>
+    /// Offers electric cooling & heating boosts
+    /// </summary>
+    class BEEHVAC:BEEBaseDevice
+    {
+
+    }
+}
