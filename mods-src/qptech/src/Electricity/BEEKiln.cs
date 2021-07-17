86b1f82e53dfcfc004a167811e8955090dbc8dca
Quentin Preik <quentin@preik.net>
2021-07-17 16:12:32 -0400

    -Fixed generator textures -electric kiln almost done, just have to add output container processing and graphics.

--- mods-src/qptech/src/Electricity/BEEKiln.cs
+++ mods-src/qptech/src/Electricity/BEEKiln.cs
@@ -22,6 +22,8 @@ namespace qptech.src
         protected double heatPerTick = 10;
         protected double insulationFactor = 0.99;
         protected double maxHeat = 2000;
+        protected double stackHeatFactor = 100; //averages up the heat over this factor
+        public double StackHeatChange => (internalheat + stackheat * (stackHeatFactor - 1)) / stackHeatFactor;
         /// <summary>
         /// Internal heat starts at resting heat
         /// While processing will increse internal heat,
@@ -46,15 +48,16 @@ namespace qptech.src
             if (Block.Attributes != null)
             {
                 //requiredFlux = Block.Attributes["requiredFlux"].AsInt(requiredFlux);
-                rmInputFace = BlockFacing.FromCode(Block.Attributes["inputFace"].AsString("up"));
+                rmInputFace = BlockFacing.FromCode(Block.Attributes["inputFace"].AsString("west"));
 
-                outputFace = BlockFacing.FromCode(Block.Attributes["outputFace"].AsString("down"));
+                outputFace = BlockFacing.FromCode(Block.Attributes["outputFace"].AsString("east"));
                 animationSpeed = Block.Attributes["animationSpeed"].AsFloat(animationSpeed);
                 rmInputFace = OrientFace(Block.Code.ToString(), rmInputFace);
                 outputFace = OrientFace(Block.Code.ToString(), outputFace);
                 heatPerTick = Block.Attributes["heatPerTick"].AsDouble(heatPerTick);
                 insulationFactor = Block.Attributes["insulationFactor"].AsDouble(insulationFactor);
                 maxHeat = Block.Attributes["maxHeat"].AsDouble(maxHeat);
+                stackHeatFactor = Block.Attributes["stackHeatFactor"].AsDouble(stackHeatFactor);
             }
             dummy = new DummyInventory(api);
         }
@@ -76,7 +79,8 @@ namespace qptech.src
             //if (Api.World.Side is EnumAppSide.Client) { return; }
             if (internalheat <= restingheat) { internalheat = restingheat;return; }
             internalheat *= insulationFactor;
-            stackheat = (stackheat + stackheat + internalheat) / 3; //BS average code lol
+            
+            stackheat = StackHeatChange; //BS average code lol
             this.MarkDirty(true);
         }
 
@@ -91,7 +95,7 @@ namespace qptech.src
                 internalheat += heatPerTick;
                 if (internalheat > maxHeat) { internalheat = maxHeat; }
             }
-            stackheat = (stackheat + stackheat + internalheat) / 3; //BS average code lol
+            stackheat = StackHeatChange; //BS average code lol
             if (stackheat >= requiredheat)
             {
                 DoDeviceComplete();
@@ -143,7 +147,7 @@ namespace qptech.src
                     if (checkiblock.CombustibleProps!=null&& checkiblock.CombustibleProps.SmeltingType == EnumSmeltType.Fire)
                     {
                         Block outputblock = Api.World.GetBlock(checkiblock.CombustibleProps.SmeltedStack.Code);
-                        Block outputitem = Api.World.GetBlock(checkiblock.CombustibleProps.SmeltedStack.Code);
+                        Item outputitem = Api.World.GetItem(checkiblock.CombustibleProps.SmeltedStack.Code);
                         
                         if (outputblock == null && outputitem==null) { continue; }
                         requiredheat = checkiblock.CombustibleProps.MeltingPoint;
@@ -158,7 +162,7 @@ namespace qptech.src
                         }
                         else
                         {
-                            inputcode = checkitem.Code.ToString();
+                            inputcode = checkiblock.Code.ToString();
                             outputcode = outputitem.Code.ToString();
                             blockoritem = "ITEM";
                         }
@@ -174,15 +178,17 @@ namespace qptech.src
                     if (checkitem.CombustibleProps != null && checkitem.CombustibleProps.SmeltingType == EnumSmeltType.Fire)
                     {
                         Block outputblock = Api.World.GetBlock(checkitem.CombustibleProps.SmeltedStack.Code);
-                        Block outputitem = Api.World.GetBlock(checkitem.CombustibleProps.SmeltedStack.Code);
-                        
+                        Item outputitem = Api.World.GetItem(checkitem.CombustibleProps.SmeltedStack.Code);
+
                         if (outputblock == null && outputitem == null) { continue; }
                         requiredheat = checkitem.CombustibleProps.MeltingPoint;
                         stackheat = restingheat;
+                        inputcode = checkitem.Code.ToString();
                         if (outputblock != null)
                         {
                             inputcode = checkitem.Code.ToString();
                             outputcode = outputblock.Code.ToString();
+
                             blockoritem = "BLOCK";
                         }
                         else
@@ -206,10 +212,29 @@ namespace qptech.src
         {
             base.GetBlockInfo(forPlayer, dsc);
 
-            dsc.AppendLine("INTERNAL TEMP :" + internalheat.ToString());
+            dsc.AppendLine("INTERNAL TEMP :" + Math.Floor(internalheat).ToString());
             dsc.AppendLine("Make :" + outputcode);
-            dsc.AppendLine("ITEMTEMP/REQ TEMP: " + stackheat.ToString()+"/"+ requiredheat.ToString());
+            dsc.AppendLine("ITEMTEMP/REQ TEMP: " + Math.Floor(stackheat).ToString()+"/"+ Math.Floor(requiredheat).ToString());
             
+        }
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve)
+        {
+            base.FromTreeAttributes(tree, worldAccessForResolve);
+            internalheat = tree.GetDouble("internalheat");
+            inputcode = tree.GetString("inputcode");
+            outputcode = tree.GetString("outputcode");
+            blockoritem = tree.GetString("blockoritem");
+            stackheat = tree.GetDouble("stackheat");
+        }
+        public override void ToTreeAttributes(ITreeAttribute tree)
+        {
+            base.ToTreeAttributes(tree);
+            
+            tree.SetDouble("internalheat", internalheat);
+            tree.SetString("inputcode", inputcode);
+            tree.SetString("outputcode", outputcode);
+            tree.SetString("blockoritem", blockoritem);
+            tree.SetDouble("stackheat", stackheat);
         }
     }
 }
